6518910b6fa6140bb4c2d5811ee3f6dc5ec0cfd1
baohongbin [鲍红彬] <baohongbin@xiaomi.com>
2014-11-24 16:34:47 +0800

    add setting test code

--- script/testcases/systemapp.py
+++ script/testcases/systemapp.py
@@ -94,6 +94,175 @@ class SystemAppTest(unittest.TestCase):
         d.press('back') 
         assert d(text="小米盒子设置").wait.exists(timeout=15000), 'exit from MiBox Setting failed!'
 
+    def testLaunchCommonSetting(self):
+        """
+        launch  app store and exit
+       """
+        for i in xrange(4):
+            d.press('right')
+            d.sleep(1)
+        assert d(text="小米盒子设置", packageName='com.xiaomi.tv.desktop').exists, 'MiBox Setting icon not found!'
+        #d(resourceId="com.xiaomi.tv.desktop:id/bottom_layer").child(index=3).child(index=1).click.wait()
+        d(text="小米盒子设置", packageName='com.xiaomi.tv.desktop').click.wait()
+        assert d(text="帐户与安全").wait.exists(timeout=5000), 'launch MiBox setting failed!'
+        assert d(text="通用设置").wait.exists(timeout=5000), 'launch MiBox setting failed!'
+        d.press('up')
+        d.sleep(1)
+        d.press('up')
+        d.sleep(1)
+        d.press('left')
+        d.sleep(1)
+        d.press('left')
+        d.sleep(1)
+        d.press('down')
+        d.sleep(1)
+        d.press('enter')
+        assert d(text="盒子系统的相关设置").wait.exists(timeout=5000), 'launch common setting failed'
+        d.press('back')
+        assert d(text="通用设置").wait.exists(timeout=5000), 'back to setting failed!'
+
+
+    def testLaunchGrapAudioSetting(self):
+        """
+        launch  app store and exit
+       """
+        for i in xrange(4):
+            d.press('right')
+            d.sleep(1)
+        assert d(text="小米盒子设置", packageName='com.xiaomi.tv.desktop').exists, 'MiBox Setting icon not found!'
+        #d(resourceId="com.xiaomi.tv.desktop:id/bottom_layer").child(index=3).child(index=1).click.wait()
+        d(text="小米盒子设置", packageName='com.xiaomi.tv.desktop').click.wait()
+        assert d(text="帐户与安全").wait.exists(timeout=5000), 'launch MiBox setting failed!'
+        assert d(text="图像与声音").wait.exists(timeout=5000), 'launch MiBox setting failed!'
+        d.press('up')
+        d.sleep(1)
+        d.press('up')
+        d.sleep(1)
+        d.press('left')
+        d.sleep(1)
+        d.press('left')
+        d.sleep(1)
+        d.press('right')
+        d.sleep(1)
+        d.press('enter')
+        assert d(text="盒子图像和声音设置").wait.exists(timeout=5000), 'launch GrapAudio setting failed'
+        d.press('back')
+        assert d(text="图像与声音").wait.exists(timeout=5000), 'back to setting failed!'
+
+    def testLaunchSecuritySetting(self):
+        """
+        launch  app store and exit
+       """
+        for i in xrange(4):
+            d.press('right')
+            d.sleep(1)
+        assert d(text="小米盒子设置", packageName='com.xiaomi.tv.desktop').exists, 'MiBox Setting icon not found!'
+        #d(resourceId="com.xiaomi.tv.desktop:id/bottom_layer").child(index=3).child(index=1).click.wait()
+        d(text="小米盒子设置", packageName='com.xiaomi.tv.desktop').click.wait()
+        assert d(text="帐户与安全").wait.exists(timeout=5000), 'launch MiBox setting failed!'
+        assert d(text="图像与声音").wait.exists(timeout=5000), 'launch MiBox setting failed!'
+        d.press('up')
+        d.sleep(1)
+        d.press('up')
+        d.sleep(1)
+        d.press('left')
+        d.sleep(1)
+        d.press('left')
+        d.sleep(1)
+        d.press('right')
+        d.sleep(1)
+        d.press('right')
+        d.sleep(1)
+        d.press('enter')
+        assert d(text="盒子系统的帐户与安全设置").wait.exists(timeout=5000), 'launch Security setting failed'
+        d.press('back')
+        assert d(text="帐户与安全").wait.exists(timeout=5000), 'back to setting failed!'
+
+    def testLaunchAboutSetting(self):
+        """
+        launch  app store and exit
+       """
+        for i in xrange(4):
+            d.press('right')
+            d.sleep(1)
+        assert d(text="小米盒子设置", packageName='com.xiaomi.tv.desktop').exists, 'MiBox Setting icon not found!'
+        #d(resourceId="com.xiaomi.tv.desktop:id/bottom_layer").child(index=3).child(index=1).click.wait()
+        d(text="小米盒子设置", packageName='com.xiaomi.tv.desktop').click.wait()
+        assert d(text="帐户与安全").wait.exists(timeout=5000), 'launch MiBox setting failed!'
+        assert d(text="关于").wait.exists(timeout=5000), 'launch MiBox setting failed!'
+        d.press('up')
+        d.sleep(1)
+        d.press('up')
+        d.sleep(1)
+        d.press('left')
+        d.sleep(1)
+        d.press('left')
+        d.sleep(1)
+        d.press('right')
+        d.sleep(1)
+        d.press('right')
+        d.sleep(1)
+        d.press('down')
+        d.sleep(1)
+        d.press('enter')
+        assert d(text="本机的相关信息").wait.exists(timeout=5000), 'launch About setting failed'
+        d.press('back')
+        assert d(text="关于").wait.exists(timeout=5000), 'back to setting failed!'
+
+    def testLaunchSystemUpdateSetting(self):
+        """
+        launch  app store and exit
+       """
+        for i in xrange(4):
+            d.press('right')
+            d.sleep(1)
+        assert d(text="小米盒子设置", packageName='com.xiaomi.tv.desktop').exists, 'MiBox Setting icon not found!'
+        #d(resourceId="com.xiaomi.tv.desktop:id/bottom_layer").child(index=3).child(index=1).click.wait()
+        d(text="小米盒子设置", packageName='com.xiaomi.tv.desktop').click.wait()
+        assert d(text="帐户与安全").wait.exists(timeout=5000), 'launch MiBox setting failed!'
+        assert d(text="系统升级").wait.exists(timeout=5000), 'launch MiBox setting failed!'
+        d.press('up')
+        d.sleep(1)
+        d.press('up')
+        d.sleep(1)
+        d.press('left')
+        d.sleep(1)
+        d.press('left')
+        d.sleep(1)
+        d.press('down')
+        d.sleep(1)
+        d.press('right')
+        d.sleep(1)
+        d.press('enter')
+        assert d(textContains="当前版本").wait.exists(timeout=5000), 'launch SystemUpdate setting failed'
+        d.press('back')
+        assert d(text="系统升级").wait.exists(timeout=5000), 'back to setting failed!'
+
+    def testLaunchNetworkSetting(self):
+        """
+        launch  app store and exit
+       """
+        for i in xrange(4):
+            d.press('right')
+            d.sleep(1)
+        assert d(text="小米盒子设置", packageName='com.xiaomi.tv.desktop').exists, 'MiBox Setting icon not found!'
+        #d(resourceId="com.xiaomi.tv.desktop:id/bottom_layer").child(index=3).child(index=1).click.wait()
+        d(text="小米盒子设置", packageName='com.xiaomi.tv.desktop').click.wait()
+        assert d(text="帐户与安全").wait.exists(timeout=5000), 'launch MiBox setting failed!'
+        assert d(text="系统升级").wait.exists(timeout=5000), 'launch MiBox setting failed!'
+        d.press('up')
+        d.sleep(1)
+        d.press('up')
+        d.sleep(1)
+        d.press('left')
+        d.sleep(1)
+        d.press('left')
+        d.sleep(1)
+        d.press('enter')
+        assert d(textContains="网络的相关设置").wait.exists(timeout=5000), 'launch Network setting failed'
+        d.press('back')
+        assert d(text="系统升级").wait.exists(timeout=5000), 'back to setting failed!'
+
     def testLaunchAndExitMediaExplorer(self):
         for i in xrange(6):
             d.press('right')
@@ -103,12 +272,12 @@ class SystemAppTest(unittest.TestCase):
         #d(resourceId="com.xiaomi.tv.desktop:id/bottom_layer").child(index=3).child(index=5).click.wait()
         #assert d(resourceId='com.xiaomi.mitv.mediaexplorer:id/entry_name', text='设备').wait.exists(timeout=10000), 'launch Media Explorer failed!'
         #assert d(resourceId='com.xiaomi.mitv.mediaexplorer:id/entry_name', text="视频").wait.exists(timeout=10000), 'launch Media Explorer failed!'
-        assert d(resourceId='com.xiaomi.mitv.mediaexplorer:id/dev', text='设备').wait.exists(timeout=15000), 'launch Media Explorer failed!'
-        assert d(resourceId='com.xiaomi.mitv.mediaexplorer:id/video', text="视频").wait.exists(timeout=15000), 'launch Media Explorer failed!'       
+        assert d(resourceId='com.xiaomi.mitv.mediaexplorer:id/dev', text='设备').wait.exists(timeout=18000), 'launch Media Explorer failed!'
+        assert d(resourceId='com.xiaomi.mitv.mediaexplorer:id/video', text="视频").wait.exists(timeout=18000), 'launch Media Explorer failed!'       
         d.press('back')
         d.sleep(3)
         d.press('back')
-        assert d(packageName='com.xiaomi.tv.desktop').wait.exists(timeout=15000), 'exit from Media Explorer failed!'
+        assert d(text="应用商店", packageName='com.xiaomi.tv.desktop').wait.exists(timeout=15000), 'exit from Media Explorer failed!'
 
     def testLaunchAndExitCloudGallery(self):
         """
